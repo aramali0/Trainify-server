e7b14e3d5e770b3e2b88c05158af27978780ead8
Aramali Mohammed <mouhammedaramali@gmail.com>
2024-10-24 00:48:51 +0200

    configure socket

--- src/main/java/e_learning/security/WebSocketConfig.java
+++ src/main/java/e_learning/security/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/wss")
                 .setAllowedOrigins(allowedOrigins)
                 // Allow the origin where your React app is running
                 .withSockJS();
